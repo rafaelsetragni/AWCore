83630e848b033a72ec2994d234a115d7790828a3
Rafael Setragni <development.support@carda.me>
2022-08-31 18:56:53 -0300

    First commit

---
+++ core/core.h
@@ -0,0 +1,17 @@
+//
+//  core.h
+//  core
+//
+//  Created by CardaDev on 30/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for core.
+FOUNDATION_EXPORT double coreVersionNumber;
+
+//! Project version string for core.
+FOUNDATION_EXPORT const unsigned char coreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <core/PublicHeader.h>
+
